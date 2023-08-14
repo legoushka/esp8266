bcd0c47b6090d71e42004c28eaf210b8c792568e
Vladislav Trunin <vladislav-trunin1@yandex.ru>
2023-08-14 13:49:38 +0300

    DHT11 sensor setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,15 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <ESP32_Supabase.h>
+#include <Adafruit_Sensor.h>
+#include "DHT.h"
 #include <secret.h>
 
 #define LED_RED D6
 #define LED_BLUE D7
+#define DHT_PIN D4
+
+#define DHTTYPE DHT11
 
 const char *supabase_url = SUPABASE_URL;
 const char *anon_key = SUPABASE_ANON_KEY;
@@ -17,10 +22,13 @@ struct Status {
   bool led_0;
   bool led_1;
   bool led_2;
-  float analog;
+  float temperature;
+  float humidity;
 };
 
-Status status = {.led_0 = false, .led_1 = false, .led_2 = false, .analog = 0.0};
+Status status = {.led_0 = false, .led_1 = false, .led_2 = false, .temperature = 0.0, .humidity = 0.0};
+
+DHT dht(DHT_PIN, DHTTYPE);
 
 Supabase db;
 
@@ -46,14 +54,18 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
-  
+  pinMode(DHT_PIN, INPUT);
+  dht.begin();
   networkSetup();
   db.begin(supabase_url, anon_key);
 }
 
 void loop() {
   JSONdoc["mac_address"] = WiFi.macAddress();
-  JSONdoc["analog_value"] = random(10);
+  Serial.println(dht.readTemperature());
+  dht.readTemperature(dht.readHumidity());
+  JSONdoc["temperature"] = dht.readTemperature();
+  JSONdoc["humidity"] = dht.readHumidity();
   JSONdoc["led_0"] = status.led_0;
   JSONdoc["led_1"] = status.led_1;
   JSONdoc["led_2"] = status.led_2;
